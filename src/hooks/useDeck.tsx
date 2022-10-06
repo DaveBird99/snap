1f39404f9531ec8cae9ab1477139ab6fc2d5207d
DaveBird99 <22491650+DaveBird99@users.noreply.github.com>
2022-10-06 18:02:52 +0100

    Added default deck

--- src/hooks/useDeck.tsx
+++ src/hooks/useDeck.tsx
@@ -8,8 +8,44 @@ export interface Card {
     suit: Suit
 }
 
-export const useDeck = () => {
-    const [deck, setDeck] = useState<Card[]>()
+const shuffle = (array: Card[]) => {
+    let currentIndex = array.length,  randomIndex;
+    
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+    
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    
+    return array;
+}
+
+const defaultDeck = (fill:Boolean = true) => {
+    if (fill) {
+        const cardNumbers = Array.from(Array(13).keys()).map(n => n+1)
+        let newDeck: Card[] = []
+        cardNumbers.forEach(number => suits.forEach(suit => {
+            const newCard: Card = {
+                number,
+                suit
+            }
+            newDeck.push(newCard)
+        }))
+        newDeck = [...newDeck, ...newDeck]
+        newDeck = shuffle(newDeck)
+        return newDeck
+    }
+    return []
+}
+
+export const useDeck = (fill:Boolean = true) => {
+    const [deck, setDeck] = useState<Card[]>(defaultDeck(fill))
 
     return {deck, setDeck}
 }
--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,13 +1,36 @@
+import {describe, expect, test} from '@jest/globals';
+import React, {useState as useStateMock, useEffect as useEffectMock} from 'react';
+import { useDeck } from "../src/hooks/useDeck"
 
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+  useEffect: jest.fn(),
+}));
+
+const setState = jest.fn();
+  const useEffect = jest.fn();
+
+  beforeEach(() => {
+    useStateMock.mockImplementation(init => [init, setState]);
+
+    useEffectMock.mockImplementation(init => [init, useEffect]);
+  });
 
 describe('useDeck first deck', () => {
-  it.todo('fills with 104 cards')
+  test('fills with 104 cards', () => {
+    const { deck } = useDeck()
+    expect(deck).toHaveLength(104)
+  })
 
   it.todo('can have top card removed')
 });
-  
-describe('useDeck second deck', () => {
-    it.todo('is empty')
 
-    it.todo('can have a card added')
+describe('useDeck second deck', () => {
+  test('is empty', () => {
+    const { deck } = useDeck(false)
+    expect(deck).toHaveLength(0)
+  })
+
+  it.todo('can have a card added')
 });
